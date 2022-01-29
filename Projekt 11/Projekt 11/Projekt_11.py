35378fd0697de614d1cee2202ce08f2ad779e6c7
MonkehDefense <74372783+MonkehDefense@users.noreply.github.com>
2022-01-29 19:41:02 +0100

    Projekt 11

--- Projekt 11/Projekt 11/Projekt_11.py
+++ Projekt 11/Projekt 11/Projekt_11.py
@@ -1,6 +1,6 @@
-from bokeh.plotting import figure
+from bokeh.plotting import figure, from_networkx
 from bokeh.io import show
-from bokeh.layouts import layout
+from bokeh.layouts import layout, column, row
 import numpy as np
 import random
 import networkx as nx
@@ -41,7 +41,7 @@ def distribution(NXgraph):
 
 def bokeh_hist(dist):
 	dist = {str(k): v for k, v in sorted(dist.items(), key=lambda item: item[0])}
-	print(dist.keys())
+	#print(dist.keys())
 	fig=figure(
 		x_range=list(dist.keys()),
 		x_axis_label='stopień',y_axis_label='ilość węzłów',
@@ -58,27 +58,38 @@ def bokeh_hist(dist):
 
 	return fig
 
+def bokeh_graph(nxgraph):
+	fig = figure(
+		title="sieć głosowania na wikipedii",
+		x_range=(-1.1,1.1), y_range=(-1.1,1.1))
+	fig.xgrid.grid_line_color = None
+	fig.toolbar.logo = None
+	fig.toolbar.autohide = True
+	fig.renderers.append(from_networkx(nxgraph,nx.spring_layout,scale=4,center=(0,0)))
+
+	return fig
+
 def main():
 	graph = make_graph('Wiki-Vote.txt')
-	nx.draw(graph)
-#	dist_deg = distribution(graph)
-#	fig = bokeh_hist(dist_deg)
-	
-#	n = 0
-#	print('Kolejne składowe spójne w sieci mają następujące ilości węzłów:')
-#	subs = list(nx.connected_components(graph))
-#	MAXsub = graph.subgraph(max(subs,key=len))
-#	for sub in subs:
-#		n+=1
-#		print(len(sub))
-#	print('\nW sumie jest',n,'składowych spójnych.\n\nZaczynam liczyć średnią najmniejszą drogę między węzłami.')
-#
-#	records = []
-#	for _ in range(1000):
-#		w1,w2 = random.choices(list(MAXsub.nodes()),k=2)
-#		records.append(nx.shortest_path_length(MAXsub,source=w1,target=w2))
-#	print('Dla największej spójnej składowej średnia najkrótsza trasa wynosi w przybliżeniu:',np.mean(records))
-#	show(fig)
+	dist_deg = distribution(graph)
+	fig1 = bokeh_hist(dist_deg)
+	fig2 = bokeh_graph(graph)
+	show(layout(row(fig1,fig2)))
+
+	n = 0
+	print('Kolejne składowe spójne w sieci mają następujące ilości węzłów:')
+	subs = list(nx.connected_components(graph))
+	MAXsub = graph.subgraph(max(subs,key=len))
+	for sub in subs:
+		n+=1
+		print(len(sub))
+	print('\nW sumie jest',n,'składowych spójnych.\n\nZaczynam liczyć średnią najmniejszą drogę między węzłami.')
+
+	records = []
+	for _ in range(1000):
+		w1,w2 = random.choices(list(MAXsub.nodes()),k=2)
+		records.append(nx.shortest_path_length(MAXsub,source=w1,target=w2))
+	print('Dla największej spójnej składowej średnia najkrótsza trasa wynosi w przybliżeniu:',np.mean(records))
 
 
 
